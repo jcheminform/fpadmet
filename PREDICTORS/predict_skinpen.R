7b59aa8935ce6b0e7a7a6017afa45080568f9488
Vishwesh Venkatraman <vishwesh.venkatraman@gmail.com>
2021-03-11 14:59:25 +0000

    Update predict_skinpen.R

--- PREDICTORS/predict_skinpen.R
+++ PREDICTORS/predict_skinpen.R
@@ -18,11 +18,11 @@ yhat <- predict(fittedmodel, newdata = X)
 if (applyadan) {
 	X <- rbind(fittedmodel$trainingData[1, 1:(ncol(fittedmodel$trainingData)-1)], X) 
 	X <- X[-1,]
-	## estimate conditional standard deviation
-	yhat_unc <- predict(fittedqrfmodel, X, what=sd, type = "se")
-	
-	Z <- data.frame(format(yhat, digits=2, nsmall=2), format(yhat_unc$se, digits=2, nsmall=2))
-	colnames(Z) <- c("Predicted", "Uncertainty")
+	# 95% predition intervals
+	yhat_unc <- predict(fittedqrfmodel, X, quantiles = c(0.025, 0.975), type="quantiles")	
+	Z <- data.frame(format(yhat, digits=2, nsmall=2), format(yhat_unc$predictions[,1], digits=2, nsmall=2), format(yhat_unc$predictions[,2], digits=2, nsmall=2))
+	colnames(Z) <- c("Predicted", "quantile_0.025", "quantile_0.975")
+
 	rownames(Z) <- rownames(X)
 	write.table(Z, file=outfile, quote = F)
 } else {
